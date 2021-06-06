b82c9e351d5491e9a56fd988f5d47ff28de10d4b
Niko Haß <nikohass571@gmail.com>
2021-06-06 10:26:50 +0200

    Remove remote testing

--- test/tasks/update.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-for filename in os.listdir("test/"):
-    path = "test/" + filename
-    if ".py" in path:
-        print(f"Updating {filename}")
-        c.request_file(path, path)
-time.sleep(30)
